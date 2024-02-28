3e82b5dc68c26b51426582d6cf0b907189bbac2e
Nazrulofficeial <hmraz101@gmail.com>
2024-02-28 13:06:14 +0600

    API post and Get Learned

--- js/index.js
+++ js/index.js
@@ -1,1 +1,15 @@
-console.log('Hello Ami ASi');
+function loadUsers2() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+        .then(res => res.json())
+        .then(data => DisplayUsers2(data));
+}
+
+function DisplayUsers2(data) {
+    const ul = document.getElementById('users-list');
+    for(const users of data){
+        console.log(users.name);
+        const li = document.createElement('li');
+        li.innerText = users.name;
+        ul.appendChild(li)
+    }
+};
